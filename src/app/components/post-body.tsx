342b8c450a51d6a426a766e05d28c5ecfa9a8c10
Caio Gouveia <caiomdm@gmail.com>
2025-04-17 15:33:56 -0300

    feat: configurar Tailwind CSS e ajustar estilos no projeto

--- src/app/components/post-body.tsx
+++ src/app/components/post-body.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import markdownStyles from "./markdown-styles.module.css";
+// import markdownStyles from "./markdown-styles.module.css";
 
 type Props = {
   content: string;
@@ -9,7 +9,7 @@ export function PostBody({ content }: Props) {
   return (
     <div className="max-w-2xl mx-auto">
       <div
-        className={markdownStyles["markdown"]}
+        // className={markdownStyles["markdown"]}
         dangerouslySetInnerHTML={{ __html: content }}
       />
     </div>
