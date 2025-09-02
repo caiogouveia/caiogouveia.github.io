a35bce41fef8dfdf17bbc4c0d4c8c667e4b131e6
Caio Gouveia <caiomdm@gmail.com>
2025-09-02 10:58:59 -0300

    adiciona politica de privacidade

---
+++ src/app/privacy-policy/page.tsx
@@ -0,0 +1,61 @@
+
+import { Container } from "@/app/components/container";
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <Container>
+      <h1 className="text-2xl font-bold mb-4">Política de Privacidade</h1>
+      <p>
+        A sua privacidade é importante para nós. É política do nosso site
+        respeitar a sua privacidade em relação a qualquer informação sua que
+        possamos coletar no site.
+      </p>
+      <h2 className="text-xl font-bold mt-4">1. Coleta de informações</h2>
+      <p>
+        Coletamos informações pessoais apenas quando realmente precisamos
+        delas para lhe fornecer um serviço. Fazemo-lo por meios justos e
+        legais, com o seu conhecimento e consentimento. Também informamos por
+        que estamos coletando e como será usado.
+      </p>
+      <h2 className="text-xl font-bold mt-4">2. Uso de dados</h2>
+      <p>
+        Apenas retemos as informações coletadas pelo tempo necessário para
+        fornecer o serviço solicitado. Quando armazenamos dados, protegemos
+        dentro de meios comercialmente aceitáveis para evitar perdas e
+        roubos, bem como acesso, divulgação, cópia, uso ou modificação não
+        autorizados.
+      </p>
+      <h2 className="text-xl font-bold mt-4">3. Cookies</h2>
+      <p>
+        O nosso site pode usar cookies para melhorar a experiência do usuário.
+        Cookies são pequenos arquivos de texto que são armazenados no seu
+        dispositivo. Você pode desativar os cookies a qualquer momento nas
+        configurações do seu navegador.
+      </p>
+      <h2 className="text-xl font-bold mt-4">
+        4. Links para sites de terceiros
+      </h2>
+      <p>
+        O nosso site pode ter links para sites externos que não são operados
+        por nós. Esteja ciente de que não temos controle sobre o conteúdo e
+        práticas desses sites e não podemos aceitar responsabilidade por suas
+        respectivas políticas de privacidade.
+      </p>
+      <h2 className="text-xl font-bold mt-4">
+        5. Alterações a esta política de privacidade
+      </h2>
+      <p>
+        Podemos atualizar nossa Política de Privacidade de tempos em tempos.
+        Aconselhamos que você revise esta página periodicamente para quaisquer
+        alterações. Iremos notificá-lo sobre quaisquer alterações, publicando
+        a nova Política de Privacidade nesta página.
+      </p>
+      <h2 className="text-xl font-bold mt-4">6. Contato</h2>
+      <p>
+        Se você tiver alguma dúvida sobre nossa Política de Privacidade, entre
+        em contato conosco.
+      </p>
+    </Container>
+  );
+}
