75932e91afb2c525497d39eba4d337d3a10c32e3
Caio Gouveia <caiomdm@gmail.com>
2026-02-22 21:04:49 -0300

    adiciona icones ao intro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,6 @@ export default function Index() {
   return (
     <main className='flex my-10 md:my-0 mx-5 md:justify-self-center md:flex-col md:w-1/2 md:h-dvh md:justify-center md:align-center'>
       <CartaApresentacao />
-      {/* <Intro /> */}
-      {/* <Cv /> */}
       {/* {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
     </main>
   );
@@ -19,9 +17,7 @@ export default function Index() {
 function CartaApresentacao() {
   return (
     <div className="leading-6">
-      <div className='mb-10'>
         <Intro />
-      </div>
       <div>
         Sou desenvolvedor com mais de 14 anos de experiência, <br/>
         atualmente na TPF - <ExLink link={'https://tpf.com.br'}>Transportadora Porto Ferreira</ExLink>. <br/>
@@ -63,33 +59,33 @@ function CartaApresentacao() {
 
 export interface ExLinkProps
 {
-  children: React.ReactNode
-  local?: boolean
-  link: string
+    children: React.ReactNode
+    local?: boolean
+    link: string
 }
 function ExLink(props: ExLinkProps){
-  const target = props.local ? "": "_blank"
-  return(
-    <span className="inline-flex flex-col w-auto h-[23px] px-2 md:px-0">
-      <span className="flex justify-center content-center">
-          <a href={props.link} target={target}>
-            {props.children}
-          </a>
-          {!props.local && (
-          <a href={props.link} style={{height: "20px",overflow: "hidden"}} target={target}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
-            <g id="Interface / External_Link">
-              <path id="Vector"
-              d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11"
-              stroke="#000000"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"/>
-            </g>
-          </svg>
-          </a>
-          )}
+    const target = props.local ? "": "_blank"
+    return(
+        <span className="inline-flex flex-col w-auto h-[23px] px-2 md:px-0">
+            <span className="flex justify-center content-center">
+                <a href={props.link} target={target}>
+                    {props.children}
+                </a>
+                {!props.local && (
+                    <a href={props.link} style={{height: "20px",overflow: "hidden"}} target={target}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none">
+                            <g id="Interface / External_Link">
+                                <path id="Vector"
+                                    d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11"
+                                    stroke="#000000"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"/>
+                            </g>
+                        </svg>
+                    </a>
+                )}
+            </span>
         </span>
-      </span>
-  );
+    );
 }
