f1025c80ee98f9013d790ebb48cdf348c0296e70
Caio Gouveia <caiomdm@gmail.com>
2026-05-20 15:33:51 -0300

    fix erros do lint

--- src/app/components/avatar.tsx
+++ src/app/components/avatar.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+
 type Props = {
   name: string;
   picture: string;
@@ -7,7 +9,13 @@ type Props = {
 const Avatar = ({ name, picture }: Props) => {
   return (
     <div className="flex items-center">
-      <img src={picture} className="w-12 h-12 rounded-full mr-4" alt={name} />
+      <Image
+        src={picture}
+        className="w-12 h-12 rounded-full mr-4"
+        alt={name}
+        width={48}
+        height={48}
+      />
       <div className="text-xl font-bold">{name}</div>
     </div>
   );
